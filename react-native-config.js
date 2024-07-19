35beab8df51bd2110cbe1406c24b8114a4c34ce2
PetrCala <cala.p@seznam.cz>
2024-07-19 13:23:58 +0200

    Modify the react-native-config

--- react-native-config.js
+++ react-native-config.js
@@ -6,6 +6,7 @@ module.exports = {
   dependencies: {
     'react-native-sqlite-storage': {
       platforms: {
+        ios: null, // Delegate the management to CocoaPods
         android: {
           sourceDir:
             '../node_modules/react-native-sqlite-storage/platforms/android-native',
