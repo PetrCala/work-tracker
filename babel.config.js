a50e94f0f8a901b3e17123fced627ff6e81f0998
PetrCala <cala.p@seznam.cz>
2024-07-19 10:02:58 +0200

    Modify the babel configuration

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,47 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: [
+    'module:metro-react-native-babel-preset',
+    ['@babel/preset-env', {targets: {node: 'current'}}],
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.native.js',
+          '.native.jsx',
+          '.native.ts',
+          '.native.tsx',
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.ios.js',
+          '.ios.jsx',
+          '.ios.ts',
+          '.ios.tsx',
+          '.android.js',
+          '.android.jsx',
+          '.android.ts',
+          '.android.tx',
+        ],
+        alias: {
+          '@assets': './assets',
+          '@auth': './src/auth',
+          '@components': './src/components',
+          '@context': './src/context',
+          '@database': './src/database',
+          '@hooks': './src/hooks',
+          '@libs': './src/libs',
+          '@navigation': './src/libs/Navigation',
+          '@screens': './src/screens',
+          '@src': './src',
+          '@storage': './src/storage',
+          '@styles': './src/styles',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
